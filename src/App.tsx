d4368cd2532ca5c0a5fb11cd7aa1e187380b25ca
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-08-15 23:26:29 -0300

    feat: add page router

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/LoginPage";
+import CreateAccount from "./pages/CreateAccount";
+import Download from "./pages/Download";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<CreateAccount />} />
+        <Route path="/download" element={<Download />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
