df63f9ab5c447f5a9463affc52ad3b9ea8dc88b0
Humberto Rodrigues <humberto7177@gmail.com>
2025-08-20 09:37:05 -0300

    feat(router): add /channel route for Channel component
    Added a new route `/channel` to the router, rendering the Channel component.
    This enables navigation to the Channel page within the application.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Home from "./pages/Home";
 import Login from "./pages/LoginPage";
 import CreateAccount from "./pages/CreateAccount";
 import Download from "./pages/Download";
+import Channel from "./pages/Channel";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/channel" element={<Channel />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<CreateAccount />} />
         <Route path="/download" element={<Download />} />
